926f3e335ba5ed3de74fe98b07a8299c6ebb38e9
ftfg777 <ftfgz5093@gmail.com>
2025-04-21 16:05:21 +0900

    style: 필터 아이콘 제거

--- src/pages/Employees/EmployeeFilters.jsx
+++ src/pages/Employees/EmployeeFilters.jsx
@@ -97,9 +97,8 @@ export function EmployeeFilters({ filters, onFilterChange }) {
             placeholder="이름 또는 이메일로 검색"
             value={filters.search}
             onChange={handleSearch}
-            className="w-full h-11 pl-10 pr-4 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-transparent text-sm text-gray-700 placeholder:text-gray-400"
+            className="w-full h-11 px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-transparent text-sm text-gray-700 placeholder:text-gray-400"
           />
-          <SearchIcon className="absolute left-3 top-3 h-5 w-5 text-gray-400" />
         </div>
         <div className="lg:col-span-3">
           <Dropdown label="부서" badge={filters.department.length || null}>
@@ -117,26 +116,20 @@ export function EmployeeFilters({ filters, onFilterChange }) {
         </div>
         <div className="lg:col-span-4 grid grid-cols-2 gap-4">
           <div className="relative">
-            <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-              <CalendarIcon size={16} className="text-gray-400" />
-            </div>
             <input
               type="date"
               value={filters.joinDateRange.from || ""}
               onChange={(e) => handleDateChange("from", e.target.value)}
-              className="w-full h-11 pl-10 pr-4 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-transparent  text-sm text-gray-700 placeholder:text-gray-400"
+              className="w-full h-11 px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-transparent  text-sm text-gray-700 placeholder:text-gray-400"
               placeholder="입사일"
             />
           </div>
           <div className="relative">
-            <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-              <CalendarIcon size={16} className="text-gray-400" />
-            </div>
             <input
               type="date"
               value={filters.joinDateRange.to || ""}
               onChange={(e) => handleDateChange("to", e.target.value)}
-              className="w-full h-11 pl-10 pr-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-transparent text-sm text-gray-700 placeholder:text-gray-400"
+              className="w-full h-11 px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-transparent text-sm text-gray-700 placeholder:text-gray-400"
               placeholder="퇴사일"
             />
           </div>
